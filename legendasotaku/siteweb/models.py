3e2fadc5eca5577e55be3e825ac61a3129005df3
fagnerpatricio <fagner.patricio@gmail.com>
2020-06-19 13:06:39 -0300

    Versao202006191306

--- legendasotaku/siteweb/models.py
+++ legendasotaku/siteweb/models.py
@@ -1,5 +1,26 @@
 from __future__ import unicode_literals
+import json
+import requests
+import xml.etree.ElementTree as ET
 from djongo import models
+
+url_imgs = 'https://cdn-eu.anidb.net/images/main/'
+
+# class SeriesManager(models.Manager):
+#     def create_series(self, titulo,temporada):
+#         serie = self.create(titulo=titulo,temporada=temporada)
+#         anime_info = (ET.parse('/home/fagner/ProjetosVSCode/anisubhub/legendasotaku/siteweb/templates/oreimo.xml')).getroot()        
+#         serie.cod_img = cod_anidb_img = '221835.jpg'
+#         open('/home/fagner/ProjetosVSCode/anisubhub/legendasotaku/siteweb/templates/img/' + cod_anidb_img, 'wb').write((requests.get(url_imgs + cod_anidb_img , allow_redirects=True).content))
+#         for episodio in anime_info.iter("episode"):
+#             for titulo in episodio.findall('title'):
+#                 if titulo.attrib['{http://www.w3.org/XML/1998/namespace}lang'] == 'en':
+#                     try:
+#                         serie.lista_episodes += ('#' + str(int(episodio.find("epno").text)) + ' - ' + titulo.text + '\n') 
+#                     except:
+#                         continue
+        
+#         return serie
 
 class Series(models.Model):
     created_at = models.DateTimeField(auto_now_add=True,auto_now=False)
@@ -7,19 +28,50 @@ class Series(models.Model):
 
     titulo = models.CharField(max_length=500,null=True)
     temporada = models.IntegerField(default=1,null=True)
-    url_the_movie_db = models.URLField(max_length = 500, blank=True)
-    url_trakt = models.URLField(max_length = 500, blank=True)
-    url_imdb = models.URLField(max_length = 500, blank=True)
-    url_tv_maze = models.URLField(max_length = 500, blank=True)
-    url_crunchyroll = models.URLField(max_length = 500, blank=True)
-    url_anidb = models.URLField(max_length = 500, blank=True)
-    url_my_anime_list = models.URLField(max_length = 500, blank=True)
-    arquivo_banner = models.ImageField(upload_to='legendasotaku/siteweb/templates/img/banners')
+    # temporada = models.IntegerField(default=1,null=True)
+    #cod_tv_maze = models.CharField(max_length = 10, null=True)
+    #url_tv_maze = models.URLField(max_length = 500, blank=True)
+    #url_the_movie_db = models.URLField(max_length = 500, blank=True)
+    #url_imdb = models.URLField(max_length = 500, blank=True)    
+    cod_img = models.CharField(max_length = 50, blank=True)    
+    lista_episodes = models.TextField(blank=True)
+    #url_trakt = models.URLField(max_length = 500, blank=True)
+    #url_crunchyroll = models.URLField(max_length = 500, blank=True)
+    #url_anidb = models.URLField(max_length = 500, blank=True)
+    #url_my_anime_list = models.URLField(max_length = 500, blank=True)
+    # objects = SeriesManager()
+
+    # arquivo_banner = models.ImageField(upload_to='legendasotaku/siteweb/templates/img/banners')
     #arquivo_do_banner = models.FilePathField(max_length=500, blank=False)
 
     def save(self, *args, **kwargs):
-        print("Hello")
+        #Codigo Para teste
+        # file = open('/home/fagner/ProjetosVSCode/anisubhub/legendasotaku/siteweb/templates/anime.json','r')        
+        # legendas = json.load(file)
+        if self.pk:
+            super(Series, self).save(*args, **kwargs)
+        else:
+            anime_info = (ET.parse('/home/fagner/ProjetosVSCode/anisubhub/legendasotaku/siteweb/templates/oreimo.xml')).getroot()
+            ##################
+            cod_anidb_img = '221835.jpg'
+            open('/home/fagner/ProjetosVSCode/anisubhub/legendasotaku/siteweb/templates/img/' + cod_anidb_img, 'wb').write((requests.get(url_imgs + cod_anidb_img , allow_redirects=True).content))
+            for episodio in anime_info.iter("episode"):
+                for titulo in episodio.findall('title'):
+                    if titulo.attrib['{http://www.w3.org/XML/1998/namespace}lang'] == 'en':
+                        try:
+                            self.lista_episodes += ('#' + str(int(episodio.find("epno").text)) + ' - ' + titulo.text + '\n') 
+                        except:
+                            continue
+            # info = requests.get('http://api.tvmaze.com/shows/' + self.cod_tv_maze + '?embed=episodes', verify=True).json()
+            # self.url_tv_maze = 'http://api.tvmaze.com/shows/' + self.cod_tv_maze
+            # self.url_imdb = 'https://www.imdb.com/title/' + str(info['externals']['imdb']) + '/'
+            # self.url_the_movie_db = 'https://www.themoviedb.org/tv/' + str(info['externals']['thetvdb'])
+            self.cod_img = cod_anidb_img
+            super(Series, self).save(*args, **kwargs) 
+        
 
     def __str__(self):
         return (self.titulo + " -- (" + str(self.temporada) + "º Temporada)")
-    
+        # return (self.titulo)
+
+# serie = Book.objects.create_book("Pride and Prejudice")
--- legendasotaku/siteweb/views.py
+++ legendasotaku/siteweb/views.py
@@ -2,11 +2,20 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Series
 
+import couchdb
+
 def index(request):
 
+    server = couchdb.Server('http://admin:123456@192.168.2.203:5984')
+    db = server['animes']  
+
+    urls_figuras = []
+    for id in db:
+        urls_figuras.append((db[id]['title'],db[id]['picture']))
     # listaDeSeries = Series.objects.filter(titulo="K-ON!") 
-    listaDeSeries = Series.objects.all()
-    for serie in listaDeSeries:
-        print(serie.url_crunchyroll) 
-    return render(request, 'index.html',{'Series': listaDeSeries})
+    # listaDeSeries = Series.objects.all()
+    # for serie in listaDeSeries:
+    #     print(serie.url_crunchyroll) 
+    # return render(request, 'index.html',{'Series': listaDeSeries})
+    return render(request, 'index.html',{ 'urls_figuras': urls_figuras })
     # return HttpResponse("Hello, world! Mundo Aberto")
